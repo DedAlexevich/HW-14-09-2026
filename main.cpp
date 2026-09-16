dfaa6e99a4ae8671e3ad4919a74fcd4946db6695
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-09-16 10:52:42 +0300

    Calc on several threads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <thread>
+#include <future>
 
 namespace kuznetsov {
   class Clicker {
@@ -23,26 +25,66 @@ namespace kuznetsov {
   };
 }
 
+using data_t = std::vector< unsigned long long >;
+using value_t = data_t::value_type;
+using const_iterator = data_t::const_iterator;
 
-int main()
+value_t summator(const_iterator b, const_iterator e)
+{
+  value_t s{0};
+  for(; b != e; ++b) {
+    s += *b;
+  }
+  return s;
+}
+
+int main(int argc, char** argv)
 {
   constexpr size_t size{1'000'000'000};
-  using data_t = std::vector< unsigned long long >;
-  using value_t = data_t::value_type;
-
-  double init{}, total{};
-  value_t sum{};
-  {
-    kuznetsov::Clicker cl;
-    data_t vals(size, 1);
-    init = cl.millisec();
-    for (size_t i = 0; i < vals.size(); ++i) {
-      sum += vals[i];
+  
+  int threads{1};
+  if (argc >= 2) {
+    threads = std::stoi(argv[1]);
+    if (threads <= 0) {
+      std::cerr << "Incorrect count of threads\n";
+      return 1;
     }
-    total = cl.millisec();
   }
-  std::cout << init << '\n';
-  std::cout << total << '\n';
-  std::cout << total - init << '\n';
+
+  size_t valsOnThread = size / threads;
+  double init{0}, total{0};
+  value_t sum{0};
+  try {
+    {
+      kuznetsov::Clicker cl;
+      data_t vals(size, 1);
+      std::vector< std::future< value_t > > tasks;
+      tasks.reserve(threads);
+      init = cl.millisec();
+
+      for(size_t i = 0; i < threads - 1; ++i) {
+        auto cbegin = vals.cbegin() + valsOnThread * i;
+        auto cend = vals.cbegin() + valsOnThread * (i + 1);
+        tasks.emplace_back(std::async(std::launch::async, summator, cbegin, cend));
+      }
+      auto cb = vals.cbegin() + valsOnThread * (threads - 1);
+      auto ce = vals.cend();
+      tasks.emplace_back(std::async(std::launch::async, summator, cb, ce));
+      
+      for(size_t i = 0; i < threads; ++i) {
+        sum += tasks[i].get();
+      }
+
+      total = cl.millisec();
+    }
+  } catch(const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  std::cout << "Init: " << init << '\n';
+  std::cout << "Total: " << total << '\n';
+  std::cout << "Duration of calc: " << total - init << '\n';
+  std::cout << "Sum: " << sum << '\n';
+  std::cout << "Threads: " << threads << '\n';
 }
 
